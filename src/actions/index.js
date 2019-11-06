14e4027bce1b48ed0637cf1a362f4a8fb7661df7
Joris Dewaele <joris.dewaele@tutanota.com>
2019-11-06 19:00:01 +0100

    Initial state action + yarn resolutions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,7 @@
 // TODO: add and export your own actions
+const initialState = {
+  currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`,
+  selectedChannel: 'general',
+  channels: ['general', 'react', 'paris'],
+  messages: []
+};
