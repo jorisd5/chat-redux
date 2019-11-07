bd46c2099fcc7d372c2d59b5eccbde4de4310da1
Joris Dewaele <joris.dewaele@tutanota.com>
2019-11-07 23:24:34 +0100

    Message component

---
+++ src/components/message.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { emojify } from 'react-emojify';
+
+const Message = (props) => {
+  const { createdAt, author, content} = props.message;
+  const time = new Date(createdAt).toLocaleTimeString();
+  return (
+    <div className="message-container">
+      <i className="author">
+        <span>{author}</span>
+        <small>{time}</small>
+      </i>
+      <p>{emojify(content)}</p>
+    </div>
+  );
+};
+export default Message;
